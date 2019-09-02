524066b2d7143c3a275d852175da0a197cc20543
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-09-02 16:59:25 +0200

    s3ve3gxx: initial bring up from klte lineage-15.1 base

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common s3ve3g
+include device/samsung/s3ve3g-common/BoardConfigCommon.mk
+
+# Shims
+TARGET_LD_SHIM_LIBS += \
+        /system/vendor/lib/libmmcamera_imx175.so|libshim_imx175.so
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_s3ve3gxx_defconfig
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm8226
+TARGET_LIBINIT_MSM8226_DEFINES_FILE := device/samsung/s3ve3gxx/init/init_s3ve3g.cpp
+
+# NFC
+# include $(COMMON_PATH)/nfc/pn547/board.mk
+
+# Radio/RIL
+include $(COMMON_PATH)/radio/single/board.mk
+
+# inherit from the proprietary version
+-include vendor/samsung/s3ve3gxx/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/samsung/s3ve3gxx/s3ve3gxx-vendor.mk)
+
+# Camera
+PRODUCT_PACKAGES += \
+    libshim_imx175
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# NFC
+# $(call inherit-product, device/samsung/s3ve3g-common/nfc/pn547/product.mk)
+
+# common s3ve3g
+$(call inherit-product, device/samsung/s3ve3g-common/s3ve3g.mk)
